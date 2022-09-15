2a3b2445301c62bd261418ceb071b1e9d39ad142
RAZIBUL HOSSEN <razibulhossen@MacBook-Air-3.local>
2022-09-15 20:00:29 +0200

    first commit

---
+++ js/debug-technique.js
@@ -0,0 +1,11 @@
+/* 
+debug steps:
+1. Error check (error reproduce)
+2. check other stuffs on the website
+3. check console for error
+4. click on the link of the error(it will take you to the code)
+5. If needed add a breakpoint and try to stop the code over there and see the values
+6. if needed console log output
+7. search full code base(cmd + shift + F) > don't forget about partial match
+8. look around for typo
+*/
